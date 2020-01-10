9c523af0cd1a073d6289d77fd0832420384315b9
Monica Djunaidi <modjun12@gmail.com>
2020-01-09 22:24:45 -0600

    no flicker on navbar when going back to home

--- src/components/shared/MyNavbar/MyNavbar.js
+++ src/components/shared/MyNavbar/MyNavbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import { Link } from 'react-router-dom';
 
 class MyNavbar extends React.Component {
   static propTypes = {
@@ -15,17 +16,37 @@ class MyNavbar extends React.Component {
 
   render() {
     const { authed } = this.props;
+    const buildNavbar = () => {
+      if (authed) {
+        return (
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/stuff">My Stuff</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/stuff/new">New Stuff</Link>
+            </li>
+            <li className="nav-item">
+              <button className="nav-link btn btn-danger" onClick={this.logMeOut}>Log Out</button>
+            </li>
+          </ul>
+        );
+      }
+
+      return (<ul className="navbar-nav ml-auto"></ul>);
+    };
+
     return (
       <div className="MyNavbar">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="/">React Hoarder</a>
+          <Link className="navbar-brand" to="/">React Hoarder</Link>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             {
-              (authed) && (<button className="btn btn-danger ml-auto" onClick={this.logMeOut}>Log Out</button>)
+              buildNavbar()
             }
           </div>
         </nav>
