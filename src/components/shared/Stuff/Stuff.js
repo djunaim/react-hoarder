fdf4895c00d21b2a5a0c195f1fb0a9aafadfb096
Monica Djunaidi <modjun12@gmail.com>
2020-01-11 13:47:33 -0600

    can delete item from myStuff

--- src/components/shared/Stuff/Stuff.js
+++ src/components/shared/Stuff/Stuff.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import itemShape from '../../../helpers/propz/itemShape';
@@ -6,19 +7,27 @@ import itemShape from '../../../helpers/propz/itemShape';
 class Stuff extends React.Component {
   static propTypes = {
     item: itemShape.itemShape,
+    deleteItem: PropTypes.func,
+  }
+
+  deleteItemEvent = (e) => {
+    e.preventDefault();
+    const { deleteItem, item } = this.props;
+    deleteItem(item.id);
   }
 
   render() {
     const { item } = this.props;
 
     return (
-      <div className="Stuff row">
-        <div className="card col-md-4">
+      <div className="Stuff col-md-4">
+        <div className="card">
           <img src={item.itemImage} alt="..." />
           <div className="card-body">
             <h4 className="card-title">{item.itemName}</h4>
             {/* <p className="card-text">{item.itemDescription}</p> */}
             <Link className="btn btn-secondary" to={`/stuff/${item.id}`}>View Single Item</Link>
+            <button className="btn btn-danger" onClick={this.deleteItemEvent}>Delete Item</button>
           </div>
         </div>
       </div>
