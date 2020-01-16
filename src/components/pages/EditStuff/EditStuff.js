963e91e5bc63f48b31ba13289b41bf346fa96a39
Monica Djunaidi <modjun12@gmail.com>
2020-01-15 18:23:14 -0600

    form prepopulates and can update info on form between items

--- src/components/pages/EditStuff/EditStuff.js
+++ src/components/pages/EditStuff/EditStuff.js
@@ -1,69 +1,97 @@
 import React from 'react';
 
+import authData from '../../../helpers/data/authData';
+import itemsData from '../../../helpers/data/itemsData';
+
 class EditStuff extends React.Component {
-state = {
-  itemName: '',
-  itemImage: '',
-  itemDescription: '',
-}
+  state = {
+    itemName: '',
+    itemImage: '',
+    itemDescription: '',
+  }
 
-nameChange = (e) => {
-  e.preventDefault();
-  this.setState({ itemName: e.target.value });
-}
+  componentDidMount() {
+    const { itemId } = this.props.match.params;
+    if (itemId) {
+      itemsData.getSingleItem(itemId)
+        .then((response) => {
+          this.setState({ itemName: response.data.itemName, itemImage: response.data.itemImage, itemDescription: response.data.itemDescription });
+        })
+        .catch((errFromEditStuff) => console.error(errFromEditStuff));
+    }
+  }
 
-imageChange = (e) => {
-  e.preventDefault();
-  this.setState({ itemImage: e.target.value });
-}
+  nameChange = (e) => {
+    e.preventDefault();
+    this.setState({ itemName: e.target.value });
+  }
 
-descriptionChange = (e) => {
-  e.preventDefault();
-  this.setState({ itemDescription: e.target.value });
-}
+  imageChange = (e) => {
+    e.preventDefault();
+    this.setState({ itemImage: e.target.value });
+  }
 
-render() {
-  const { itemName, itemImage, itemDescription } = this.state;
+  descriptionChange = (e) => {
+    e.preventDefault();
+    this.setState({ itemDescription: e.target.value });
+  }
 
-  return (
-      <form className="EditStuff">
-        <div className="form-group">
-          <label htmlFor="item-name">Item Name</label>
-          <input
-            input="text"
-            className="form-control"
-            id="item-name"
-            placeholder="Enter item name"
-            value={itemName}
-            onChange={this.nameChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="item-image">Item Image</label>
-          <input
-            input="text"
-            className="form-control"
-            id="item-image"
-            placeholder="Enter item image url"
-            value={itemImage}
-            onChange={this.imageChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="item-description">Item Description</label>
-          <input
-            input="text"
-            className="form-control"
-            id="item-description"
-            placeholder="Enter item description"
-            value={itemDescription}
-            onChange={this.descriptionChange}
-          />
-        </div>
-        <button className="btn btn-success" onClick={this.saveItemEvent}>Save Item</button>
-      </form>
-  );
-}
+  updateItemEvent = (e) => {
+    e.preventDefault();
+    const { itemId } = this.props.match.params;
+    const editedItem = {
+      itemName: this.state.itemName,
+      itemDescription: this.state.itemDescription,
+      itemImage: this.state.itemImage,
+      uid: authData.getUid(),
+    };
+    itemsData.updateItem(itemId, editedItem)
+      .then(() => this.props.history.push('/stuff'))
+      .catch((errFromUpdateItem) => console.error(errFromUpdateItem));
+  }
+
+  render() {
+    const { itemName, itemImage, itemDescription } = this.state;
+
+    return (
+        <form className="EditStuff">
+          <div className="form-group">
+            <label htmlFor="item-name">Item Name</label>
+            <input
+              input="text"
+              className="form-control"
+              id="item-name"
+              placeholder="Enter item name"
+              value={itemName}
+              onChange={this.nameChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="item-image">Item Image</label>
+            <input
+              input="text"
+              className="form-control"
+              id="item-image"
+              placeholder="Enter item image url"
+              value={itemImage}
+              onChange={this.imageChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="item-description">Item Description</label>
+            <input
+              input="text"
+              className="form-control"
+              id="item-description"
+              placeholder="Enter item description"
+              value={itemDescription}
+              onChange={this.descriptionChange}
+            />
+          </div>
+          <button className="btn btn-info" onClick={this.updateItemEvent}>Update Item</button>
+        </form>
+    );
+  }
 }
 
 export default EditStuff;
